97a78a58a9ceb636cee1719d8e94c595dcf72c9e
EricPollert <112413919+EricPollert@users.noreply.github.com>
2022-10-09 23:29:22 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+  int resistance [5] = {16, 27, 39, 56, 81};
+  double current [5];
+  double power[5];
+
+  for (int i = 0; i <= 5; i++)
+  {
+
+    
+  }
+}
